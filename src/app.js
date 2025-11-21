e59185a6ecbb8b9839721c528bb589c9eeb05171
Maikoeliud <maikoeliud@gmail.com>
2025-11-21 06:58:22 +0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
+// import cors from "cors";
 const express = require("express");
+const cors = require("cors");
 const userRoutes = require("./routes/users");
 const transactionRoutes = require("./routes/transactions");
 const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // Allow requests from this origin (adjust if your frontend port changes)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 app.use(express.json());
 
 app.use("/users", userRoutes);
 app.use("/transactions", transactionRoutes);
+
+// Enable CORS for your frontend origin
 
 app.use(errorHandler); // Must be last
 
