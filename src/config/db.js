a363094a1576c2a21da1f57dfc1f8a32bcb38de3
Maikoeliud <maikoeliud@gmail.com>
2025-11-26 19:17:45 +0300

    db adjusted

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,24 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
+const pg = require("pg");
+
 // Determine environment
 const isProduction =
   process.env.NODE_ENV === "production" || process.env.VERCEL;
 
 let sequelize;
 
-if (isProduction) {
+// if (isProduction) {
   // Production: Use DATABASE_URL from Neon
   console.log("🌐 Connecting to production database (Neon)...");
-
+console.log(
+  "✅ pg loaded successfully — version:",
+  require("pg/package.json").version
+);
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectModule: pg,
     logging: false, // Set to console.log for debugging
     dialectOptions: {
       ssl: {
@@ -27,28 +33,28 @@ if (isProduction) {
       idle: 10000,
     },
   });
-} else {
+// } else {
   // Local Development: Use individual DB credentials
-  console.log("💻 Connecting to local database...");
+  // console.log("💻 Connecting to local database...");
 
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST || "localhost",
-      port: process.env.DB_PORT || 5432,
-      dialect: "postgres",
-      logging: false, // Set to console.log for debugging
-      pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000,
-      },
-    }
-  );
-}
+  // sequelize = new Sequelize(
+  //   process.env.DB_NAME,
+  //   process.env.DB_USER,
+  //   process.env.DB_PASSWORD,
+  //   {
+  //     host: process.env.DB_HOST || "localhost",
+  //     port: process.env.DB_PORT || 5432,
+  //     dialect: "postgres",
+  //     logging: false, // Set to console.log for debugging
+  //     pool: {
+  //       max: 5,
+  //       min: 0,
+  //       acquire: 30000,
+  //       idle: 10000,
+  //     },
+  //   }
+  // );
+// }
 
 // Test connection
 sequelize
