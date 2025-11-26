7eb5d0aa46598c1cce27d5d77a986cb9e3e14322
Maikoeliud <maikoeliud@gmail.com>
2025-11-26 18:19:49 +0300

    credentials

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const errorHandler = require("./middleware/errorHandler");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://micro-savings-frontend.vercel.app/"], // Allow requests from this origin (adjust if your frontend port changes)
+    origin: [
+      "http://localhost:5173",
+      "https://micro-savings-frontend.vercel.app/",
+    ], // Allow requests from this origin (adjust if your frontend port changes)
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
